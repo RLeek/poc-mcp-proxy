805a744ee0a1f03dabf09604599628eba427a0ce
Ricky Liu <rky.w.liu@relevanceai.com>
2025-03-22 21:34:04 +1100

    Add tool_ids

--- src/utilities.ts
+++ src/utilities.ts
@@ -34,19 +34,19 @@ export const consts = () => {
         throw new Error('RELEVANCE_REGION is not set')
     }
     const REGION = process.env.RELEVANCE_REGION
-    /*
+    
     if (!process.env.TOOL_IDS) {
         console.error("TOOL_IDS is not set")
         throw new Error(JSON.stringify(process.env))
     }
     const TOOL_IDS = process.env.TOOL_IDS.split(' ');
-    */
+    
     const BASE_API_URL = `https://api-${REGION}.stack.tryrelevance.com/latest`
 
     constsCache = {
         TOKEN: TOKEN,
         REGION: REGION,
-        TOOL_IDS: ["1211fa18-50a3-414e-a0ed-567a1a1c66dc","83a672f6-42db-4cab-a1a4-d4e2270321fd"],
+        TOOL_IDS: TOOL_IDS,
         BASE_API_URL: BASE_API_URL
     }
     return constsCache
