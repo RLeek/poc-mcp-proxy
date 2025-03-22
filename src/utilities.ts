2ad0f45ef87c08eb8891c80350ac9735cbc0d14b
Ricky Liu <rky.w.liu@relevanceai.com>
2025-03-22 21:27:50 +1100

    Test

--- src/utilities.ts
+++ src/utilities.ts
@@ -22,30 +22,31 @@ export const consts = () => {
     if (constsCache) {
         return constsCache
     }
+    /*
     if (!process.env.RELEVANCE_AUTH_TOKEN) {
         console.error("RELEVANCE_AUTH_TOKEN is not set")
         throw new Error('RELEVANCE_AUTH_TOKEN is not set')
     }
     const TOKEN = process.env.RELEVANCE_AUTH_TOKEN
-      
+    */
     if (!process.env.RELEVANCE_REGION) {
         console.error("RELEVANCE_REGION is not set")
         throw new Error('RELEVANCE_REGION is not set')
     }
     const REGION = process.env.RELEVANCE_REGION
-      
+    /*
     if (!process.env.TOOL_IDS) {
         console.error("TOOL_IDS is not set")
         throw new Error(JSON.stringify(process.env))
     }
     const TOOL_IDS = process.env.TOOL_IDS.split(' ');
-
+    */
     const BASE_API_URL = `https://api-${REGION}.stack.tryrelevance.com/latest`
 
     constsCache = {
-        TOKEN: TOKEN,
+        TOKEN: "f79c3c1a258f-4144-afa8-7cf7dab7eb78:sk-NTdkYWViYzYtNjhmZC00ZTdlLWI5NmEtODVjOGVmYjMwNGU2",
         REGION: REGION,
-        TOOL_IDS: TOOL_IDS,
+        TOOL_IDS: ["1211fa18-50a3-414e-a0ed-567a1a1c66dc","83a672f6-42db-4cab-a1a4-d4e2270321fd"],
         BASE_API_URL: BASE_API_URL
     }
     return constsCache
